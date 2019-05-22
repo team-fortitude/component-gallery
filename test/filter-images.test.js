40a9e3a67cc035cd2922c7c48bae5c9c53cc8c29
Dan Hogan <hogan.daniel@hotmail.com>
2019-05-22 16:33:06 -0700

    moved filterImages to own folder and imported back into test

--- test/filter-images.test.js
+++ test/filter-images.test.js
@@ -1,3 +1,5 @@
+import filterImages from '../src/filter-images.js';
+
 const test = QUnit.test;
 
 QUnit.module('Filter Images');
@@ -25,19 +27,6 @@ const images = [
         'horns': 1
     }
 ];
-
-function filterImages(filter, images) {
-    const lowerCaseFilter = filter.toLowerCase();
-    const filteredTitles = images.filter(image => {
-        const lowerCaseTitle = image.title.toLowerCase();
-        const textFilter = lowerCaseTitle.includes(lowerCaseFilter);
-        const hornFilter = image.horns.toString() === lowerCaseFilter;
-
-        return textFilter || hornFilter;
-    });
-    return filteredTitles;
-}
-
 
 test('Searches based on text in title', (assert) => {
     // Arrange
